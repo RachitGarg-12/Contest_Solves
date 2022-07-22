058b0aa31b997eecc5a5206613da615a6e612df9
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:37:32 +0530

    Committing files

---
+++ CodeChef/C++17/AVGOF3/57111714.cpp
@@ -0,0 +1,26 @@
+//Rachit Garg
+#include <iostream>
+#include <string>
+#define fo(i, n) for (i = 0; i < n; i++)
+#include<bits/stdc++.h>
+#define fa(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define pb push_back
+#define rfo(i, n) for (ll i = n - 1; i >= 0; i--)
+#define ll long long
+#define asort(a) sort(a.begin(), a.end())
+#define dsort(a) sort(a.begin(), a.end(), greater<int>())
+using namespace std;
+   
+    
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int x;cin>>x;
+        int a= (3*x)-3;
+        cout<<1<<" "<<2<<" "<<a<<endl;
+    }
+	return 0;
+}
