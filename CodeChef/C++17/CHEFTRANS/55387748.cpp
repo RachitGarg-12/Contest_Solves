1a0df02fe52b13e2cb9989b387034ca80f3aedfc
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:37:38 +0530

    Committing files

---
+++ CodeChef/C++17/CHEFTRANS/55387748.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#define fo(i, n) for (i = 0; i < n; i++)
+#include<bits/stdc++.h>
+#define fa(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define pb push_back
+#define rfo(i, n) for (ll i = n - 1; i >= 0; i--)
+#define ll long long
+#define asort(a) sort(a.begin(), a.end())
+#define dsort(a) sort(a.begin(), a.end(), greater<int>())
+using namespace std;
+   
+    
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;cin>>x>>y>>z;
+        if(x+y<z){cout<<"PLANEBUS"<<endl;}
+        else if(x+y==z){cout<<"EQUAL"<<endl;}
+        else{cout<<"TRAIN"<<endl;}
+    }
+	return 0;
+}
