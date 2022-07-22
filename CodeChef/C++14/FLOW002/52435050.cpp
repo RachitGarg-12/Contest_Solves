c0d24daaf825c3932af098ff614ca0679c394f25
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:41:09 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW002/52435050.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+      int t;cin>>t;
+      while(t--){
+          int a,b;
+          cin>>a>>b;
+          int ans;
+          ans= a%b;
+          cout<<ans<<endl;
+      }
+
+	return 0;
+}
