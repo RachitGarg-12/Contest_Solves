aabda2446e0ca7fc47d4e78351bf045eaf56ed8e
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:39:23 +0530

    Committing files

---
+++ CodeChef/C++17/NUMGAME/52598566.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        if(n%2==0) cout<<"ALICE"<<endl;
+        else cout<<"BOB"<<endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
