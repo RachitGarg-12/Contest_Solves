79828b70466fea677fd7a16517c051a91b6c465d
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:41:04 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW001/49911568.cpp
@@ -0,0 +1,20 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	cin>>T;
+	while(T--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	}
+
+	
+
+	return 0;
+}
