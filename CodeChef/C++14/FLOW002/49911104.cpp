bc4334264cd7ac495734a1718fced17d75bdefb0
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:41:10 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW002/49911104.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>=b){cout<<a%b<<endl;}
+	    else{cout<<a<<endl;}
+	    
+	}
+
+	return 0;
+}
