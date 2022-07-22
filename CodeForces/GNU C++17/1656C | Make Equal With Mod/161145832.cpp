c78d93e0b8c90d368317e49c5142db5817ccc23e
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:50:20 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1656C | Make Equal With Mod/161145832.cpp
@@ -0,0 +1,143 @@
+     //Rachit Garg
+    #include<bits/stdc++.h>
+    using namespace std;
+    #define ll long long 
+    #define all(arr) arr.begin(), arr.end()
+    #define ce  cout<<endl
+    #define nl  "\n"
+    #define fo(i, n) for (ll i = 0; i < n; i++)
+    #define f1(i, n) for (ll i = 1; i <=n; i++)
+    #define fab(a,b) for (ll i = a; i < b; i++)
+    #define rfo(i, n) for (ll i = n - 1; i >= 0; i--)
+    #define pb push_back
+    #define asort(a) sort(a.begin(), a.end())
+    #define dsort(a) sort(a.begin(), a.end(), greater<int>())
+    #define min3(a, b, c) min(c, min(a, b))
+    #define min4(a, b, c, d) min(d, min(c, min(a, b)))
+    #define max3(a, b, c) max(c, max(a, b))
+    #define max4(a, b, c, d) max(d, max(c, max(a, b)))
+    #define mod 998244353
+    typedef unsigned long long ull;
+    typedef long double lld;
+    #ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+    #else
+    #define debug(x)
+    #endif
+    
+    void _print(ll t) {cerr << t;}
+    void _print(int t) {cerr << t;}
+    void _print(string t) {cerr << t;}
+    void _print(char t) {cerr << t;}
+     void _print(lld t) {cerr << t;}
+    void _print(double t) {cerr << t;}
+    void _print(ull t) {cerr << t;}
+    
+    template <class T, class V> void _print(pair <T, V> p);
+    template <class T> void _print(vector <T> v);
+    template <class T> void _print(set <T> v);
+    template <class T, class V> void _print(map <T, V> v);
+    template <class T> void _print(multiset <T> v);
+    template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+    template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+    template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+    ll lcm(ll  a, ll b)
+    {
+        return (a/__gcd(a,b))*b ;
+    }
+    
+    bool ch(string s,string a){
+        ll pos=0;
+        ll n=s.length();ll o=a.length();
+        for(ll i=0;i<n;i++){
+            if(pos==o){pos=0;}
+            if(a[pos]!=s[i]){return false;}
+            pos++;
+        }
+        return true;
+    }
+    bool prime(int n){
+        if(n<0){
+            return false;
+        }
+        for(int i =2;i*i<n;i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool palindrome(int n){
+        int r,sum = 0,k;
+        k = n;
+        while(n>0){
+        r = n%10;
+        sum = sum * 10 + r;
+        n = n/10;
+    }
+        if( k == sum){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+ 
+    bool isPalindrome(string S)
+    { for (int i = 0; i < S.length() / 2; i++) {
+            if (S[i] != S[S.length() - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    pair<ll,ll> powg2(ll n){
+    ll i,ans;
+        for(i=1;i<=32;i++){
+            ll m = n*((ll)1<<i);
+           if(m>32768){ans=n*((ll)1<<(i-1));break;}
+        }
+        return {ans,i-1};
+        
+    } 
+ 
+    /////////////////////////////////////   
+        
+    int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        // #ifndef ONLINE_JUDGE    
+        // freopen("input.txt", "r", stdin); 
+        // freopen("output.txt", "w", stdout);
+        // freopen("error.txt", "w", stderr);
+        // #endif 
+      
+        ll t;cin>>t;
+        while(t--){
+            ll n;cin>>n;bool b=false;ll ev=0;
+            ll a[n];fo(i,n){cin>>a[i];if(a[i]==1){b=true;}
+            if(a[i]%2==0){ev++;}}
+            sort(a,a+n);
+            // we can make mod of all elements 0 if we keep mod with highest element till 1st
+            //we will get mod 0 for all
+            // if we have 1 present then we try to make all mod 1 ,
+            // we try to mod all highest elements with a[i]-1 , if(a[i-1]=a[i]-1){not possible}
+            if(b==false){cout<<"YES"<<nl;}
+            else{
+                bool poss=true;
+                for(ll i=n-1;i>0;i--){
+                    if(a[i]==a[i-1]+1){poss=false;}
+                }
+                  if(poss==false){cout<<"NO"<<nl;}
+                  else{cout<<"YES"<<nl;}
+            }
+ 
+ 
+            }
+       
+ 
+            return 0;
+        }
