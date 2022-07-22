7ab1a489a75fdc8a76e80871be531f2a4878d0c8
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:37:57 +0530

    Committing files

---
+++ CodeChef/C++17/EQUALCOIN/53692977.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x==0){
+            if(y%2==1){cout<<"NO"<<endl;}
+            else{cout<<"YES"<<endl;}
+        }
+        else{
+        if(x%2==1){cout<<"NO"<<endl;}
+        else{cout<<"YES"<<endl;}
+        }
+    }
+	
+	return 0;
+}
