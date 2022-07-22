f03b542d6cf26ab4557043834a84f60266f773bb
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:40:47 +0530

    Committing files

---
+++ CodeChef/C++14/FSQRT/49957765.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    int ans=sqrt(n);
+	    cout<<ans<<endl;
+	}
+	return 0;
+	
+}
