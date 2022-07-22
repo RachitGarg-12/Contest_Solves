0fdca7e32cf7707567c3d52fe670d595d8234904
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:39:43 +0530

    Committing files

---
+++ CodeChef/C++14/DIFFSUM/50022555.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+int b;
+int s,d;
+cin>>a;
+cin>>b;
+s=a+b;
+d=a-b;
+if(a>b)
+{
+ cout<<d<<endl;
+}
+else
+{
+    cout<<s<<endl;
+}
+	return 0;
+}
