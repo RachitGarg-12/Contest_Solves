574e9bba98c74d7d042baa844936b5cde56e3326
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:59:20 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1682A | Palindromic Indices/158066000.cpp
@@ -0,0 +1,105 @@
+     //Rachit Garg
+    #include<bits/stdc++.h>
+    using namespace std;
+    #define ll long long 
+    #define all(arr) arr.begin(), arr.end()
+    #define ce  cout<<endl
+    #define fo(i, n) for (ll i = 0; i < n; i++)
+    #define f1(i, n) for (ll i = 1; i <=n; i++)
+    #define fab(a,b) for (ll i = a; i < b; i++)
+    #define rfo(i, n) for (ll i = n - 1; i >= 0; i--)
+    #define pb push_back
+    #define asort(a) sort(a.begin(), a.end())
+    #define dsort(a) sort(a.begin(), a.end(), greater<int>())
+    #define min3(a, b, c) min(c, min(a, b))
+    #define min4(a, b, c, d) min(d, min(c, min(a, b)))
+    #define max3(a, b, c) max(c, max(a, b))
+    #define max4(a, b, c, d) max(d, max(c, max(a, b)))
+    #define mod 998244353
+ 
+    ll lcm(ll  a, ll b)
+    {
+        return (a/__gcd(a,b))*b ;
+    }
+    
+    bool ch(string s,string a){
+        ll pos=0;
+        ll n=s.length();ll o=a.length();
+        for(ll i=0;i<n;i++){
+            if(pos==o){pos=0;}
+            if(a[pos]!=s[i]){return false;}
+            pos++;
+        }
+        return true;
+    }
+    bool prime(int n){
+        if(n<0){
+            return false;
+        }
+        for(int i =2;i*i<n;i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool palindrome(int n){
+        int r,sum = 0,k;
+        k = n;
+        while(n>0){
+        r = n%10;
+        sum = sum * 10 + r;
+        n = n/10;
+    }
+        if( k == sum){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+ 
+    bool isPalindrome(string S)
+    { for (int i = 0; i < S.length() / 2; i++) {
+            if (S[i] != S[S.length() - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    pair<ll,ll> powg2(ll n){
+    ll i,ans;
+        for(i=1;i<=32;i++){
+            ll m = n*((ll)1<<i);
+           if(m>32768){ans=n*((ll)1<<(i-1));break;}
+        }
+        return {ans,i-1};
+        
+    } 
+ 
+    /////////////////////////////////////   
+        
+    int main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+       /* #ifndef ONLINE_JUDGE    
+        freopen(input.txt, r, stdin); 
+        freopen(output.txt, w, stdout);
+        #endif */
+      
+        ll t;cin>>t;
+        while(t--){
+           ll n;cin>>n;
+           string s;cin>>s;
+           ll ans=0;ll st=(n-1)/2;
+          if(n%2==1){ans++;}
+          else{ ans += 2;}
+           for(ll i=st;i>0;i--){
+              if(s[i]==s[i-1]){ans += 2;}
+              else{break;}
+           }
+            cout<<ans<<endl;
+ 
+            }
+            return 0;
+        }
