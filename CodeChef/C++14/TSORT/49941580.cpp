9d0a1fec953813c33f20c6bba06f08bdc97b6609
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:40:52 +0530

    Committing files

---
+++ CodeChef/C++14/TSORT/49941580.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    int a[t];
+    for(int i=0;i<t;i++){
+        
+        cin>>a[i];
+}  
+    sort(a,a+t);
+    for(int i=0;i<t;i++){
+        cout<<a[i]<<endl;
+    }
+    	return 0;
+}
+   
+    
+    
+        
+    
+    
+    
+	
+
