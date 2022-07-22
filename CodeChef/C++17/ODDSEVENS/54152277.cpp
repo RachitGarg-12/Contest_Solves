fe620ac91f0669476eb28d6c68b931d7025a8849
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:38:33 +0530

    Committing files

---
+++ CodeChef/C++17/ODDSEVENS/54152277.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        if((m+n)%2==0) cout<<"Bob"<<endl;
+        else cout<<"Alice"<<endl;
+        
+    }
+	
+	return 0;
+}
