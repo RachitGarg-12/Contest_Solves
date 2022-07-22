3bad2e58643253e4a6c3fe62581fe85827828202
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-22 20:40:36 +0530

    Committing files

---
+++ CodeChef/C++14/REMISS/49987675.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>b){
+	        cout<<a<<" " <<a+b<<endl;
+	    }else{
+	        cout<<b<<" " <<a+b<<endl;
+	    }
+	}
+	return 0;
+}
+
